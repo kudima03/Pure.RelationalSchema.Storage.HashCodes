902bf855518dfb32481746fe9eccb89422fa8c1d
Dmitry Kurochkin <kudima03@gmail.com>
2025-11-04 11:36:09 +0300

    Breaking changes satisfied.

--- src/Pure.RelationalSchema.Storage.HashCodes/CellHash.cs
+++ src/Pure.RelationalSchema.Storage.HashCodes/CellHash.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using Pure.HashCodes;
+using Pure.HashCodes.Abstractions;
 using Pure.RelationalSchema.Storage.Abstractions;
 
 namespace Pure.RelationalSchema.Storage.HashCodes;
